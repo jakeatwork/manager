0a3c1a2e95b74c5e2c0a9513473421b838ad3dc8
Jake Levine <jake@agileramen.com>
2016-10-18 20:14:36 -0700

    Add initial reducer and store config with basic setup and components to render new UI

--- src/app.js
+++ src/app.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
 import { Header } from './components/common';
 
-const App = () => {
-  return (
-    <Provider store={createStore(reducers)}>
-      <View style={{ flex: 1 }}>
-        <Header headerText="Tech Stack" />
-        
-      </View>
-    </Provider>
-  );
-};
+class App extends Component {
+  render() {
+    return (
+      <Provider store={createStore(reducers)}>
+        <View>
+          <Text>
+            Hello!
+          </Text>
+        </View>
+      </Provider>
+    );
+  }
+}
 
 export default App;
