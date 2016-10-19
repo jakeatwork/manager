0ad062f16c504b184f398ce663d8c64d9597f7d0
Jake Levine <jake@agileramen.com>
2016-10-18 20:19:28 -0700

    Add firebase module and initial config

--- src/app.js
+++ src/app.js
@@ -2,10 +2,22 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
 import reducers from './reducers';
 import { Header } from './components/common';
 
 class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyDon1H9elXmZbIuzgZY-lnImzjNXUcq16A',
+      authDomain: 'manager-16c2b.firebaseapp.com',
+      databaseURL: 'https://manager-16c2b.firebaseio.com',
+      storageBucket: 'manager-16c2b.appspot.com',
+      messagingSenderId: '693402752674'
+    };
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
       <Provider store={createStore(reducers)}>
